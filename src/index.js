ce5ae4afcf388bce5dcfcc7d7613722dad5f8d1d
Phillip Dodson <clevergadget@gmail.com>
2019-05-13 13:05:49 -0400

    added webpack immer and a gitignore, init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import produce from "immer"
+
+const baseState = [
+    {
+        todo: "Learn typescript",
+        done: true
+    },
+    {
+        todo: "Try immer",
+        done: false
+    }
+]
+
+const nextState = produce(baseState, draftState => {
+    draftState.push({todo: "Tweet about it"})
+    draftState[1].done = true
+})
